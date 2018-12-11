336052ee3a493ba3e761dbf98b6796be1fbbca86
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-12-11 11:31:44 +0700

    Add validation to registration form

--- app/src/main/java/a59070087/kmitl/ac/th/mobilefinal/RegisterFragment.java
+++ app/src/main/java/a59070087/kmitl/ac/th/mobilefinal/RegisterFragment.java
@@ -7,12 +7,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterFragment extends Fragment {
     public static final String TAG = "REGISTER";
 
     private EditText _userId, _name, _age, _password;
+    private Button _registerBtn;
+    private String errUserId, errName, errAge, errPwd;
 
     @Nullable
     @Override
@@ -26,13 +30,81 @@ public class RegisterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         Log.d(TAG, "onActivityCreated");
         registerComponents();
+
+        errUserId = getString(R.string.register_userId_error);
+        errName = getString(R.string.register_name_error);
+        errAge = getString(R.string.register_age_error);
+        errPwd = getString(R.string.register_pwd_error);
+
+        initRegisterBtn();
     }
 
-    public void registerComponents() {
+    private void registerComponents() {
         Log.d(TAG, "registerComponents");
         _userId = getView().findViewById(R.id.register_inp_user_id);
         _name = getView().findViewById(R.id.register_inp_name);
         _age = getView().findViewById(R.id.register_inp_age);
         _password = getView().findViewById(R.id.register_inp_pwd);
+        _registerBtn = getView().findViewById(R.id.register_btn_register);
+    }
+
+    private void initRegisterBtn() {
+        Log.d(TAG, "initRegisterBtn");
+        _registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "initRegisterBtn: onClick");
+
+                final String USER_ID = _userId.getText().toString();
+                final String NAME = _name.getText().toString();
+                String AGE = _age.getText().toString();
+                final String PASSWORD = _password.getText().toString();
+
+                int errCount = 0;
+
+                if (AGE.isEmpty()) AGE = "0";
+                
+                if (!isUserValid(USER_ID)) {
+                    Toast.makeText(getContext(), errUserId, Toast.LENGTH_SHORT).show();
+                    errCount += 1;
+                }
+
+                if (!isNameValid(NAME)) {
+                    Toast.makeText(getContext(), errName, Toast.LENGTH_SHORT).show();
+                    errCount += 1;
+                }
+
+                if (!isAgeValid(Integer.parseInt(AGE))) {
+                    Toast.makeText(getContext(), errAge, Toast.LENGTH_SHORT).show();
+                    errCount += 1;
+                }
+
+                if (!isPasswordValid(PASSWORD)) {
+                    Toast.makeText(getContext(), errPwd, Toast.LENGTH_SHORT).show();
+                    errCount += 1;
+                }
+
+                if (errCount == 0) {
+                    // proceed
+                }
+            }
+        });
+    }
+
+    private boolean isUserValid(String userId) {
+        return userId.length() >= 6 && userId.length() <= 12;
+    }
+
+    private boolean isNameValid(String name) {
+        final int SPACES = name.length() - name.replaceAll(" ", "").length();
+        return SPACES == 1;
+    }
+
+    private boolean isAgeValid(int age) {
+        return age >= 10 && age <= 80;
+    }
+
+    private boolean isPasswordValid(String pwd) {
+        return pwd.length() > 6;
     }
 }
