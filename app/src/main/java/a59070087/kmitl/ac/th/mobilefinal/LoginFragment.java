797c841513da5e32ce52578efc37d6af23dd584a
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-12-11 11:05:19 +0700

    Go to RegisterFragment when clicking on a link in LoginFragment

--- app/src/main/java/a59070087/kmitl/ac/th/mobilefinal/LoginFragment.java
+++ app/src/main/java/a59070087/kmitl/ac/th/mobilefinal/LoginFragment.java
@@ -3,6 +3,7 @@ package a59070087.kmitl.ac.th.mobilefinal;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
+    public static final String TAG = "LOGIN";
+
     private EditText _userId, _password;
     private Button _loginBtn;
     private TextView _registerLink;
@@ -20,21 +23,25 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated");
         registerComponents();
 
         pleaseFillIn = getString(R.string.please_fill_in);
         noCredentialFound = getString(R.string.invalid_user_or_pwd);
 
         initLoginBtn();
+        initRegisterLink();
     }
 
     private void registerComponents() {
+        Log.d(TAG, "registerComponents");
         _userId = getView().findViewById(R.id.login_inp_user);
         _password = getView().findViewById(R.id.login_inp_pwd);
         _loginBtn = getView().findViewById(R.id.login_btn_login);
@@ -42,9 +49,13 @@ public class LoginFragment extends Fragment {
     }
 
     private void initLoginBtn() {
+        Log.d(TAG, "initLoginBtn");
+
         _loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "initLoginBtn: onClick");
+
                 final String USER_ID = _userId.getText().toString();
                 final String PASSWORD = _password.getText().toString();
 
@@ -53,6 +64,23 @@ public class LoginFragment extends Fragment {
                 } else {
                     Toast.makeText(getContext(), noCredentialFound, Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+    }
+
+    private void initRegisterLink() {
+        Log.d(TAG, "initRegisterLink");
+
+        _registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "initRegisterLink: onClick");
+
+                getFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.main_view, new RegisterFragment())
+                        .commit();
             }
         });
     }
--- app/src/main/java/a59070087/kmitl/ac/th/mobilefinal/RegisterFragment.java
+++ app/src/main/java/a59070087/kmitl/ac/th/mobilefinal/RegisterFragment.java
@@ -3,27 +3,33 @@ package a59070087.kmitl.ac.th.mobilefinal;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class RegisterFragment extends Fragment {
+    public static final String TAG = "REGISTER";
+
     private EditText _userId, _name, _age, _password;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated");
         registerComponents();
     }
 
     public void registerComponents() {
+        Log.d(TAG, "registerComponents");
         _userId = getView().findViewById(R.id.register_inp_user_id);
         _name = getView().findViewById(R.id.register_inp_name);
         _age = getView().findViewById(R.id.register_inp_age);
